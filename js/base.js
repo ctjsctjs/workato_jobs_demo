d134234bcfeadbaf6663916e763c6373a86ee4f8
ctjsctjs <ctjsctjs@gmail.com>
2018-07-24 14:37:59 +0800

    Implemented API call for job listing

--- js/base.js
+++ js/base.js
@@ -1,7 +1,22 @@
 console.log('hello world');
+var baseURL = 'https://ctjsctjs.github.io/workato_jobs_demo/';
 
 $.get( "https://boards-api.greenhouse.io/v1/boards/workatodemo/jobs",
 function( data ) {
-  $( ".result" ).html( data.jobs );
-  console.log(data.jobs['0'].id);
+
+  console.log(data);
+
+  $.each(data['jobs'], function(i, item) {
+
+    var id = item['id'];
+    var title = item['title'];
+    var url = item['absolute_url'];
+    var link = baseURL  + '?gh_jid=' + id;
+    var content = '<li>'
+    + title
+    + '<a href="' + link + '"/a>' + 'Apply' + '</a>'
+    + '</li>'
+
+    $( ".job_list_ul" ).append(content);
+  });
 });
