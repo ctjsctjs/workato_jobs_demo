8c35e36389daa2bd92a0729e6fc822867b63a2eb
ctjsctjs <ctjsctjs@gmail.com>
2018-07-24 16:46:52 +0800

    Add apply apge

--- js/base.js
+++ js/base.js
@@ -1,22 +1,29 @@
-console.log('hello world');
-var baseURL = 'https://ctjsctjs.github.io/workato_jobs_demo/';
+//var baseURL = 'https://ctjsctjs.github.io/workato_jobs_demo/';
+var baseURL = '/apply/';
 
-$.get( "https://boards-api.greenhouse.io/v1/boards/workatodemo/jobs",
-function( data ) {
-
-  console.log(data);
-
-  $.each(data['jobs'], function(i, item) {
-
-    var id = item['id'];
-    var title = item['title'];
-    var url = item['absolute_url'];
-    var link = baseURL  + '?gh_jid=' + id;
-    var content = '<li>'
-    + title
-    + '<a href="' + link + '"/a>' + 'Apply' + '</a>'
-    + '</li>'
-
-    $( ".job_list_ul" ).append(content);
-  });
+$( document ).ready(function() {
+    loadData();
 });
+
+function loadData(){
+  $.get( "https://boards-api.greenhouse.io/v1/boards/workatodemo/jobs",
+  function( data ) {
+
+    console.log(data);
+
+    $.each(data['jobs'], function(i, item) {
+
+      var id = item['id'];
+      var title = item['title'];
+      var url = item['absolute_url'];
+      var link = baseURL  + '?gh_jid=' + id;
+      var content = '<li>'
+      + title
+      + '<a href="' + link + '"/a>' + 'Apply' + '</a>'
+      + '</li>'
+
+      $( ".job_list_ul" ).append(content);
+    });
+  });
+
+}
